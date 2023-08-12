e8682bd95f3806fefb62095ca7f9e068ad3d8d3e
Jedidiah Koh <jedidiahkkh@gmail.com>
2023-08-12 23:22:35 +0800

    add textures

--- src/glsl/shader.fs
+++ src/glsl/shader.fs
@@ -1,7 +1,13 @@
 #version 330 core
 in vec3 ourColor;
+in vec2 texCoord;
 out vec4 FragColor;
+
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+uniform float mixAmt;
+
 void main()
 {
-  FragColor = vec4(ourColor, 1.0);
+  FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), mixAmt) * vec4(ourColor, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,29 @@
 #include <iostream>
 #include "shader.h"
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
-  glViewport(0, 0, width, height);
-}
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
-void processInput(GLFWwindow* window) {
-  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-    glfwSetWindowShouldClose(window, true);
-}
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void processInput(GLFWwindow* window);
 
-float vertices[] = {
-  -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
-  0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
-  0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
-};
-// float vertices[] = { 0.5f,  0.5f,  0.0f,
-//                     0.5f,  -0.5f, 0.0f,
-//                     -0.5f, -0.5f, 0.0f,
-//                     -0.5f, 0.5f,  0.0f };
+// float vertices[] = {
+//   -0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f,
+//   0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,
+//   0.0f, 0.5f, 0.0f, 0.0f, 0.0f, 1.0f
+// };
+float vertices[] = { 0.5f,  0.5f,  0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+                    0.5f,  -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+                    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+                    -0.5f, 0.5f,  0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f };
 
-// unsigned int indices[] = { 0, 1, 3, 1, 2, 3 };
+unsigned int indices[] = { 0, 1, 3, 1, 2, 3 };
 
 // float vertices2[] = { 0.0f, 1.0f, 0.0f,
 //                       1.0f, 1.0f, 0.0f,
 //                       1.0f, 0.0f, 0.0f };
+
+float mixAmt = 0.2f;
 
 int main() {
   glfwInit();
@@ -55,23 +54,70 @@ int main() {
 
   Shader shaderProgram("glsl/shader.vs", "glsl/shader.fs");
 
+  // set up textures
+  stbi_set_flip_vertically_on_load(true);
+  unsigned int texture1, texture2;
+
+  glGenTextures(1, &texture1);
+  glBindTexture(GL_TEXTURE_2D, texture1);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  int width, height, nChannels;
+  unsigned char* data = stbi_load("assets/container.jpg", &width, &height, &nChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
+
+  glGenTextures(1, &texture2);
+  glBindTexture(GL_TEXTURE_2D, texture2);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  data = stbi_load("assets/awesomeface.png", &width, &height, &nChannels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  }
+  else {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
+
+  shaderProgram.use();
+  shaderProgram.setInt("texture1", 0);
+  glUniform1i(glGetUniformLocation(shaderProgram.ID, "texture2"), 1);
+  glBindTexture(GL_TEXTURE_2D, 0);
+
+  // set up shapes
   unsigned int EBO, VBO, VAO;
   glGenVertexArrays(1, &VAO);
   glBindVertexArray(VAO);
 
-  // glGenBuffers(1, &EBO);
-  // glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-  // glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
-  //   GL_STATIC_DRAW);
+  glGenBuffers(1, &EBO);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+    GL_STATIC_DRAW);
 
   glGenBuffers(1, &VBO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
 
   glBindVertexArray(0);
 
@@ -103,9 +149,16 @@ int main() {
     // glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
 
     // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+    shaderProgram.setFloat("mixAmt", mixAmt);
     glBindVertexArray(VAO);
-    // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    // glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindTexture(GL_TEXTURE_2D, 0);
     glBindVertexArray(0);
 
     glfwSwapBuffers(window);
@@ -114,4 +167,17 @@ int main() {
 
   glfwTerminate();
   return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+  else if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+    mixAmt = fmin(1.0f, mixAmt + 0.001f);
+  else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+    mixAmt = fmax(0.0f, mixAmt - 0.001f);
 }
